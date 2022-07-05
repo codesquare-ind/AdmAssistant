41a249c491efd5a39acf0ce48b7f4dcf223c7e0d
bhanupsingh <developer.net.11@gmail.com>
2022-07-05 23:22:31 +0530

    site integration

--- Site/settings.py
+++ Site/settings.py
@@ -24,10 +24,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-xe7r&zuw1scx11qh)obx(d)v432k2n^#uzn2e=z8so$bdun0vs'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['0.0.0.0','localhost', '127.0.0.1','https://admissionsquare.in','admissionsquare.in','https://fra1.digitaloceanspaces.com/admassistant']
-
+CSRF_TRUSTED_ORIGINS=['https://admissionsquare.in',]
 
 # Application definition
 
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
     'Site',
     'Connect',
 ]
+SITE_ID = 1
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
